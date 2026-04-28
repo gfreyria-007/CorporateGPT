25ad8a995626e1c87a159b68b44701b14ffebcfd
gabrielfreyria-bit <gabrielfreyria@gmail.com>
2026-04-28 17:08:15 -0600

    chore: Update Firebase config for corporategptv2

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,21 @@
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDibbDHl11Dp54dMZiFIUR8CKegxqtMkT4",
+  authDomain: "corporategptv2.firebaseapp.com",
+  projectId: "corporategptv2",
+  storageBucket: "corporategptv2.firebasestorage.app",
+  messagingSenderId: "282195596392",
+  appId: "1:282195596392:web:c479ed0ff71dc349de57dd",
+  measurementId: "G-7D88LWN0KT"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
